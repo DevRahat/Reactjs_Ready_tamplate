748fe1cfbfdad13829d9873abef56ad5d25e14ec
DevRahat <dev.rahatislam@gmail.com>
2024-03-25 23:38:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { IconName } from "react-icons/fa6";
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>React Project Ready template</h1>
+     <IconName></IconName>
+    
+    </>
+  )
+}
+
+export default App
